bbd93b79f0fe7b07ae59a48f6516b9568d7f173a
Karthik Prasad <adkp.mlr@gmail.com>
2024-06-14 21:28:58 +0530

    FIX: app3 not rendering on docker

--- app3/src/App.tsx
+++ app3/src/App.tsx
@@ -1,22 +1,11 @@
 import React from 'react';
-import { BrowserRouter, HashRouter, Route, Routes, useRoutes } from 'react-router-dom';
-import { createRoot } from 'react-dom/client';
+import { useRoutes } from 'react-router-dom';
 import routes from './routes';
 
 import "./index.scss";
-import Home from './modules/home/home';
-import App3 from './modules/app3/app3';
+
 
 export default function App() {
-  // const content = useRoutes(routes);
-  // return content;
-  return (
-    <HashRouter>
-        <Routes>
-        <Route path='/' element={<Home />}>
-          <Route index element={<App3 />} />
-        </Route>
-        </Routes>
-    </HashRouter>
-  );
+  const content = useRoutes(routes);
+  return content;
 };
--- app3/src/bootstrap.tsx
+++ app3/src/bootstrap.tsx
@@ -2,7 +2,12 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from './App';
+import { HashRouter } from 'react-router-dom';
 
 const container = document.getElementById("app");
 const root = createRoot(container!);
-root.render(<App />);
+root.render(<React.StrictMode>
+    <HashRouter>
+        <App />
+    </HashRouter>
+  </React.StrictMode>);
